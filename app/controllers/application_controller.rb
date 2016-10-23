a90e4b75264390694408c1d8ad6ebec1b0b6da9a
eebasadre <edsil@piktochart.com>
2016-10-23 19:08:08 +0800

    Setup devise and doorkeeper for authentication and authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :doorkeeper_authorize! 
+
+  def current_resource_owner
+    
+    binding.pry
+    User.find( params[:id] ) #if doorkeeper_token
+  end
 end
