aa16e4e4505d4a1d5025b943c9f86798a458f731
Pepeng Agimat <eebasadre20@gmail.com>
2016-12-02 00:00:58 +0800

    Add spec for password grant type (#9)

--- spec/controllers/api/oauth/tokens_controller_spec.rb
+++ spec/controllers/api/oauth/tokens_controller_spec.rb
@@ -52,7 +52,55 @@ describe Api::Oauth::TokensController do
           end
         end
       end
+
+      describe 'password' do
+        context 'when valid client and user credentials' do
+          before do
+            request.headers['HTTP_GRANT_TYPE'] = 'password'
+            request.headers['HTTP_CLIENT_ID'] = app.uid
+            request.headers['HTTP_CLIENT_SECRET'] = app.secret
+
+            post :create, email: user.email, password: user.password
+          end
+
+          it 'successfully responds authentication details' do
+            expect( response ).to have_http_status( 200 )
+            expect( json ).to include_json(
+              success: true,
+              data: {
+                auth: {
+                  expires_in: 86400,
+                  scope: "public",
+                  token_type: "bearer"
+                }
+              }
+             )
+            expect( json['data']['auth']['access_token'] ).to_not be_nil
+            expect( json['data']['auth']['access_token'] ).to_not be_nil
+          end
+        end
+
+        context 'when invalid client and user credentials' do
+          before do
+            request.headers['HTTP_GRANT_TYPE'] = 'password'
+            request.headers['HTTP_CLIENT_ID'] = 'invalid app uid'
+            request.headers['HTTP_CLIENT_SECRET'] = 'invalid app secret'
+
+            post :create, email: 'incorrect email', password: 'incorrect password'
+          end
+
+          it 'responds authentication failed' do
+            expect( response ).to have_http_status( 401 )
+            expect( json ).to include_json(
+              success: false,
+              errors: [ {
+                error: 'invalid_client',
+                error_description: 'Client authentication failed due to unknown client, no client authentication included, or unsupported authentication method.'
+              } ]
+             )
+          end
+        end
+      end
     end
-    
   end
 end
