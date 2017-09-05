4b066ce502af850828c141665393cab1e047c469
Edsil Basadre <eebasadre20@gmail.com>
2017-09-05 13:40:05 +0800

    added(): User role when they register.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,8 @@
-class Api::V1::UsersController < ApplicationController
+  class Api::V1::UsersController < ApplicationController
   def create
     user = User.new( user_params )
 
     if user.save
-      user.add_role( params[:user][:role] )
-
       application = Doorkeeper::Application.where( 
                         uid: Rails.application.secrets.doorkeeper['client_id'], 
                         secret: Rails.application.secrets.doorkeeper['client_secret']
@@ -18,9 +16,9 @@ class Api::V1::UsersController < ApplicationController
                         })
 
       render json: { user_id: oauth_resource.id,
-                     token: oauth_resource.token,
-                     refresh_token: oauth_resource.refresh_token,
-                     expires_in: oauth_resource.expires_in 
+                      token: oauth_resource.token,
+                      refresh_token: oauth_resource.refresh_token,
+                      expires_in: oauth_resource.expires_in 
                     }, status: :created
     else
       render json: user.errors, status: :unproccessable_entity
@@ -44,10 +42,10 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit( :email, :password, :role )
+    params.permit( :email, :password, roles_attributes: [:name]  )
   end
 
   def account_params
     params.permit( :email )
   end
-end
+  end
