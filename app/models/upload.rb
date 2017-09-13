ea4a886818b3681cb12b9cb4a8cad8f0e9462ae1
Edsil Basadre <eebasadre20@gmail.com>
2017-09-13 13:13:58 +0800

    modify(): Refactor naming of scope on finding user clearances and certificates.

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,6 @@
 class Upload < ApplicationRecord
   belongs_to :uploadable, polymorphic: true
 
-  scope :user_clearances, -> ( clearances ) { where( id: clearances ) }
-  scope :user_certifcates, -> ( certificates ) { where( id: certificates) }
+  scope :find_user_clearances, -> ( clearances ) { where( id: clearances ) }
+  scope :find_user_certificates, -> ( certificates ) { where( id: certificates) }
 end
